73e5257949f9c225b293aba4ecd50495d433bcbf
ManoloEsS <manolo.estrada.s@gmail.com>
2026-08-14 18:18:00 -0600

    org mode not fully

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,28 +1,44 @@
 return {
   {
-    'nvim-orgmode/orgmode',
-    ft = 'org',
+    "nvim-orgmode/orgmode",
+    ft = "org",
+    keys = {
+      {
+        "<leader>oa",
+        function()
+          require("orgmode").action("agenda.prompt")
+        end,
+        desc = "Org agenda",
+      },
+      {
+        "<leader>oc",
+        function()
+          require("orgmode").action("capture.prompt")
+        end,
+        desc = "Org capture",
+      },
+    },
     dependencies = {
       {
-        'nvim-orgmode/org-bullets.nvim',
+        "nvim-orgmode/org-bullets.nvim",
         opts = {},
       },
     },
     config = function()
-      require('orgmode').setup({
-        org_agenda_files = '~/org/**/*',
-        org_default_notes_file = '~/org/refile.org',
+      require("orgmode").setup({
+        org_agenda_files = "~/org/**/*",
+        org_default_notes_file = "~/org/refile.org",
         org_capture_templates = {
           d = {
-            description = 'Dev journal entry',
-            template = '**** %^{Topic}\n***** %U %?',
-            target = '~/org/dev-journal.org',
+            description = "Dev journal entry",
+            template = "**** %^{Topic}\n***** %U %?",
+            target = "~/org/dev-journal.org",
             datetree = true,
           },
         },
         org_babel_default_header_args = {
-          [':tangle'] = 'no',
-          [':noweb'] = 'no',
+          [":tangle"] = "no",
+          [":noweb"] = "no",
         },
       })
     end,
